01ff38614c0ca4b70d7b81a76583a5eefa039da6
Jay <1426955281@qq.com>
2019-05-24 00:55:18 +0800

    add picture transfer

--- app/src/main/java/mammoth/agv/MainActivity.java
+++ app/src/main/java/mammoth/agv/MainActivity.java
@@ -15,9 +15,11 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -30,10 +32,16 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imageCamera;
+    Bitmap bmp;
+    boolean isRev = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,14 +67,222 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         value = read.getString("port", "");
         EditText etPort = findViewById(R.id.textPort);
         etPort.setText(value);
+        final Timer timer = new Timer();
+        TimerTask task;
+
+        Switch switch_isrev = (Switch) findViewById(R.id.switchUpdate);
+        // 添加监听
+        switch_isrev.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    isRev = true;
+                }else {
+                    isRev = false;
+                }
+            }
+        });
+
+        //计时器任务，每隔100ms尝试获取图片
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                if (!isRev) return;
+
+                String outputStr;
+                final String endline = new String("\n********************");
+                outputStr = "开始连接...";
+                Message Msg1 = new Message();
+                Msg1.obj = outputStr;
+                handlerOutput.sendMessage(Msg1);
+                new Thread() {
+                    @Override
+                    public void run() {
+                        Looper.prepare();
+                        try {
+                            String outputStr = new String();
+                            final String endline = new String("\n********************");
+                            outputStr = "开始连接...";
+                            Message Msg1 = new Message();
+                            Msg1.obj = outputStr;
+                            handlerOutput.sendMessage(Msg1);
+
+                            outputStr = "获取数据...";
+                            Message Msg2 = new Message();
+                            Msg2.obj = outputStr;
+                            handlerOutput.sendMessage(Msg2);
+
+                            EditText etX = findViewById(R.id.textX);
+                            EditText etY = findViewById(R.id.textY);
+                            EditText etHost = findViewById(R.id.textHost);
+                            EditText etPort = findViewById(R.id.textPort);
+
+                            if (TextUtils.isEmpty(etHost.getText())) {
+                                outputStr = "host不能为空！" + endline;
+                                Message Msg311 = new Message();
+                                Msg311.obj = outputStr;
+                                handlerOutput.sendMessage(Msg311);
+                                return;
+                            }
+                            String hostText = String.valueOf(etHost.getText()).trim();
+                            boolean hostflag = true;
+                            int hostcnt = 0, dotcnt = 0;
+                            for (int i = 0; i < hostText.length(); ++i) {
+                                if (hostcnt > 3 || dotcnt > 3) {
+                                    hostflag = false;
+                                    break;
+                                } else if (hostText.charAt(i) == '.') {
+                                    if (i == 0 || i == hostText.length() - 1) {
+                                        hostflag = false;
+                                        break;
+                                    }
+                                    hostcnt = 0;
+                                    ++dotcnt;
+                                    continue;
+                                } else if (!Character.isDigit(hostText.charAt(i))) {
+                                    hostflag = false;
+                                    break;
+                                } else {
+                                    ++hostcnt;
+                                }
+                            }
+                            if (dotcnt != 3 || hostcnt > 3) hostflag = false;
+                            if (!hostflag) {
+                                outputStr = "host格式错误！" + endline;
+                                Message Msg312 = new Message();
+                                Msg312.obj = outputStr;
+                                handlerOutput.sendMessage(Msg312);
+                                return;
+                            }
+                            SharedPreferences.Editor editor = getSharedPreferences("HostAndPort", MODE_PRIVATE).edit();
+                            editor.putString("host", hostText);
+                            editor.commit();
+
+                            if (TextUtils.isEmpty(etPort.getText())) {
+                                outputStr = "port不能为空！" + endline;
+                                Message Msg32 = new Message();
+                                Msg32.obj = outputStr;
+                                handlerOutput.sendMessage(Msg32);
+                                return;
+                            }
+
+                            String portText = String.valueOf(etPort.getText()).trim();
+                            if(!isNumeric(portText)) {
+                                outputStr = "端口数据错误，应为整数！" + endline;
+                                Message Msg321 = new Message();
+                                Msg321.obj = outputStr;
+                                handlerOutput.sendMessage(Msg321);
+                                return;
+                            }
+                            if(Integer.valueOf(portText) < 0 || Integer.valueOf(portText) > 65535) {
+                                outputStr = "端口范围错误！0<=port<=65535！" + endline;
+                                Message Msg322 = new Message();
+                                Msg322.obj = outputStr;
+                                handlerOutput.sendMessage(Msg322);
+                                return;
+                            }
+                            editor.putString("port", portText);
+                            editor.commit();
+
+                            if (TextUtils.isEmpty(etX.getText()) || TextUtils.isEmpty(etY.getText())) {
+                                outputStr = "坐标不能为空！" + endline;
+                                Message Msg33 = new Message();
+                                Msg33.obj = outputStr;
+                                handlerOutput.sendMessage(Msg33);
+                                return;
+                            }
+
+                            InetAddress addr = InetAddress.getByName(hostText);
+                            Socket socket = new Socket(addr, Integer.valueOf(portText));
+
+                            OutputStream os = socket.getOutputStream();
+                            String sendmsg = "xx" + etX.getText().toString().trim();
+                            os.write(sendmsg.getBytes());
+                            os.flush();
+                            outputStr = "发送消息：" + sendmsg;
+
+                            sendmsg = "yy" + etY.getText().toString().trim();
+                            os.write(sendmsg.getBytes());
+                            os.flush();
+                            socket.shutdownOutput();
+                            outputStr += sendmsg;
+
+                            Message Msg4 = new Message();
+                            Msg4.obj = outputStr;
+                            handlerOutput.sendMessage(Msg4);
+
+                            outputStr = "开始接收图片...";
+                            Message Msg5 = new Message();
+                            Msg5.obj = outputStr;
+                            handlerOutput.sendMessage(Msg5);
+
+                            DataInputStream dataInput = new DataInputStream(socket.getInputStream());
+
+                            List<Byte> list = new ArrayList<Byte>();
+                            int size = 1024;
+                            byte[] data = new byte[size];
+                            int len = 0;
+
+                            while ((len = dataInput.read(data, 0, data.length)) > 0)
+                            {
+                                // 重要：把length作为参数传进去，
+                                // 由于网络原因，DataInputStream.read并不是每次都返回byteCount个字节，有可能比byteCount小，
+                                // 那么inputByte[]后面的字节填充的是无效数据，要把它忽略掉.否则。图片拿过来有马赛克
+                                appendByteArrayIntoByteList(list, data, len);
+                            }
+
+                            Byte[] IMG = (Byte[]) list.toArray(new Byte[0]);
+                            byte[] img = new byte[IMG.length];
+                            for (int i = 0; i < IMG.length; i++)
+                            {
+                                img[i] = IMG[i];
+                            }
+
+                            ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+
+                            Bitmap bmp = BitmapFactory.decodeByteArray(img, 0, img.length);
+                            bmp.compress(Bitmap.CompressFormat.PNG, 100, outPut);
+
+                            Message msg = new Message();
+                            //msg.what = 0x002;
+                            msg.obj = bmp;
+                            handler.sendMessage(msg);
+
+                            outputStr = "图片已接收！";
+                            Message Msg6 = new Message();
+                            Msg6.obj = outputStr;
+                            handlerOutput.sendMessage(Msg6);
+
+                            os.close();
+                            socket.close();
+
+                            outputStr = endline;
+                            Message Msg0 = new Message();
+                            Msg0.obj = outputStr;
+                            handlerOutput.sendMessage(Msg0);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Looper.loop();
+                    }
+                }.start();
+            }
+        };
+        //启动定时器
+        timer.schedule(task, 100, 100);
     }
 
     Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
+            //super.handleMessage(msg);
             Bitmap model = (Bitmap) msg.obj;
             ImageView imageView = findViewById(R.id.imageCamera);
-            imageView.setImageBitmap(model);
+            //imageView.setImageBitmap(model);
+            //imageView.setImageBitmap(bmp);
+
+            Bitmap dst = Bitmap.createScaledBitmap(model, imageView.getHeight(), imageView.getWidth(), true);
+            imageView.setImageBitmap(dst);
         }
     };
 
@@ -83,6 +299,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     };
+
+    private Handler myHandler = new Handler()
+    {
+        public void handleMessage(android.os.Message msg)
+        {
+
+            super.handleMessage(msg);
+
+            ImageView view = (ImageView) findViewById(R.id.imageCamera);
+            view.setImageBitmap(bmp);
+
+        };
+    };
+
     @Override
     public void onClick(View v) {
         new Thread() {
@@ -201,6 +431,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Msg4.obj = outputStr;
                     handlerOutput.sendMessage(Msg4);
 
+                    /*
                     InputStream is = socket.getInputStream();
                     InputStreamReader reader = new InputStreamReader(is);
                     BufferedReader bufReader = new BufferedReader(reader);
@@ -212,6 +443,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Message Msg5 = new Message();
                     Msg5.obj = ("接受消息：" + sb.toString());
                     handlerOutput.sendMessage(Msg5);
+                    */
                     /*
                     runOnUiThread(new Runnable() {
                         @Override
@@ -248,10 +480,114 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     msg.what = 0x002;
                     msg.obj = bmp;
                     handler.sendMessage(msg);
-*/
+                    */
+                    outputStr = "开始接收图片...";
+                    Message Msg5 = new Message();
+                    Msg5.obj = outputStr;
+                    handlerOutput.sendMessage(Msg5);
+
+                    DataInputStream dataInput = new DataInputStream(socket.getInputStream());
+
+                    List<Byte> list = new ArrayList<Byte>();
+                    //int size = dataInput.readInt();
+                    int size = 1024;
+                    byte[] data = new byte[size];
+                    int len = 0;
+                    /*
+                    while (len < size) {
+                        len += dataInput.read(data, len, size - len);
+                    }
+                    */
+
+                    while ((len = dataInput.read(data, 0, data.length)) > 0)
+                    {
+                        // 重要：把length作为参数传进去，
+                        // 由于网络原因，DataInputStream.read并不是每次都返回byteCount个字节，有可能比byteCount小，
+                        // 那么inputByte[]后面的字节填充的是无效数据，要把它忽略掉.否则。图片拿过来有马赛克
+                        appendByteArrayIntoByteList(list, data, len);
+                    }
+
+                    Byte[] IMG = (Byte[]) list.toArray(new Byte[0]);
+                    byte[] img = new byte[IMG.length];
+                    for (int i = 0; i < IMG.length; i++)
+                    {
+                        img[i] = IMG[i];
+                    }
+
+                    ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+
+                    //Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                    Bitmap bmp = BitmapFactory.decodeByteArray(img, 0, img.length);
+                    bmp.compress(Bitmap.CompressFormat.PNG, 100, outPut);
+
+                    Message msg = new Message();
+                    //msg.what = 0x002;
+                    msg.obj = bmp;
+                    handler.sendMessage(msg);
+
+                    outputStr = "图片已接收！";
+                    Message Msg6 = new Message();
+                    Msg6.obj = outputStr;
+                    handlerOutput.sendMessage(Msg6);
+
+                    //myHandler.obtainMessage().sendToTarget();
+                    /*
+                    int length = 0;
+                    DataInputStream dis = null;
+                    try
+                    {
+                        try
+                        {
+                            List<Byte> list = new ArrayList<Byte>();
+
+                            dis = new DataInputStream(socket.getInputStream());
+
+                            byte[] inputByte = new byte[1024];
+                            System.out.println("开始接收数据...");
+                            while ((length = dis.read(inputByte, 0, inputByte.length)) > 0)
+                            {
+                                // 重要：把length作为参数传进去，
+                                // 由于网络原因，DataInputStream.read并不是每次都返回byteCount个字节，有可能比byteCount小，
+                                // 那么inputByte[]后面的字节填充的是无效数据，要把它忽略掉.否则。图片拿过来有马赛克
+                                appendByteArrayIntoByteList(list, inputByte, length);
+                            }
+
+                            Byte[] IMG = (Byte[]) list.toArray(new Byte[0]);
+
+                            byte[] img = new byte[IMG.length];
+                            for (int i = 0; i < IMG.length; i++)
+                            {
+                                img[i] = IMG[i];
+                            }
+
+
+                            ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+                            bmp = BitmapFactory.decodeByteArray(img, 0, img.length);
+                            bmp.compress(Bitmap.CompressFormat.PNG, 100, outPut);
+
+                            //myHandler.obtainMessage().sendToTarget();
+
+
+                        } catch (Exception e)
+                        {
+                            e.printStackTrace();
+                        } finally
+                        {
+
+                            if (dis != null)
+                                dis.close();
+                            if (socket != null)
+                                socket.close();
+                        }
+                    } catch (Exception e)
+                    {
+
+                    }
+                    */
+
+
                     os.close();
                     socket.close();
-
 
                     outputStr = endline;
                     Message Msg0 = new Message();
@@ -271,6 +607,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             float x = event.getX() / linearLayoutCamera.getWidth();
             float y = event.getY() / linearLayoutCamera.getHeight();
             x *= 1000;
+
             y *= 1000;
             if (x < 0) x = 0;
             if (y < 0) y = 0;
@@ -307,6 +644,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         return true;
+    }
+
+    private void appendByteArrayIntoByteList(List<Byte> list, byte[] array, int count)
+    {
+        for (int ii = 0; ii < count; ++ii)
+        {
+            list.add(array[ii]);
+        }
+        return;
     }
 }
 
